53a2742ecb07a640d3b21876668452e673824582
ravikantpal1408 <ravikantpal123@gmail.com>
2025-08-13 09:50:21 +0530

    worked on package and modules

---
+++ PythonCore/py_local_pkg/py_module.py
@@ -0,0 +1,5 @@
+def database_connection(query):
+    print('Connecting to database')
+    print(f'executing the query: {query}')
+    print('got the result set')
+    print('closing the db connection')
