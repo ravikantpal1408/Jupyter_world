07e572ea938c4e5fce6b7a182323303c25a94234
ravi <ravikantpal123@gmail.com>
2025-08-16 05:22:23 +0530

    moddules and packages completed

---
+++ my_game/game_01/leaderboard.py
@@ -0,0 +1,2 @@
+def show_leaderboard():
+    print("Showing leaderboard...")
---
+++ my_game/main.py
@@ -0,0 +1,5 @@
+from game_01.leaderboard import show_leaderboard
+from game_01.play import start_game
+
+start_game()
+show_leaderboard()
